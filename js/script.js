c83976e21bc1d95f61ffb7ab4e43a702dec556ac
eduard <eduard.teixeira@adsrock.com.br>
2020-01-17 01:58:19 -0300

    lista de marcas valores unicos sem repetir

--- js/script.js
+++ js/script.js
@@ -18,8 +18,7 @@ function onInit() {
 
             var listBrands = [];
 
-            console.log("LISTA DE PRODUTOS....");
-            console.log(listProducts);
+            var uniqueBrands = [];
 
             for (i = 0; i < listProducts.length; i++) {
 
@@ -36,26 +35,29 @@ function onInit() {
                 listBrands.push(listProducts[i].brand);
             }
 
-            for (i = 0; i < listBrands.length; i++) {
 
-                for (j = 0; j < listBrands.length; j++) {
+            $.each(listBrands, function (i, el) {
+                if ($.inArray(el, uniqueBrands) === -1) uniqueBrands.push(el);
+            });
 
-                    if (listBrands[i] == listBrands[j]) {
-                        console.log(i + " ..... " + j);
-                        //console.log("ITEM i... " + i + "... j..." + j + " listBrands[i] + listBrands[j]");
-                        //listBrands = listBrands.splice(j, 0);
-                        listBrands.splice(j, 1);
-                    }
+            console.log("MARCAS.... 31 diferentes ... TEM ::: " + uniqueBrands.length);
 
-                }
+            uniqueBrands.sort();
+            for (i = 0; i < uniqueBrands.length; i++) {
+                document.getElementById("brands").innerHTML += `
+                <li>
+                    <input type="checkbox" value="` + uniqueBrands[i] + `"> 
+                    ` + uniqueBrands[i] + `
+                </li>`;
             }
 
-            console.log("MARCAS....");
-            console.log(listBrands)
         },
         error: function (error) {
+
             console.log("ERROR");
+
             console.log(error);
+
             document.getElementById("myTable").innerHTML += `
             <tr>
                 <td>Erro ao buscar produtos, recarregue a página e tente novamente.</td>
@@ -64,105 +66,190 @@ function onInit() {
             </tr>`;
         },
         complete: function () {
+
             document.getElementById("content").style.display = "block";
+
             document.getElementById("loading").style.display = "none";
         }
     });
 }
 
-function sortTable(n) {
+function sortByString(n) {
+
     var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
+
     table = document.getElementById("myTable");
+
     switching = true;
+
     dir = "asc";
 
     while (switching) {
+
         switching = false;
+
         rows = table.rows;
+
         for (i = 1; i < (rows.length - 1); i++) {
+
             shouldSwitch = false;
+
             x = rows[i].getElementsByTagName("td")[n];
+
             y = rows[i + 1].getElementsByTagName("td")[n];
+
             if (dir == "asc") {
+
                 if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+
                     shouldSwitch = true;
+
                     break;
+
                 }
+
             } else if (dir == "desc") {
+
                 if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+
                     shouldSwitch = true;
+
                     break;
+
                 }
+
             }
+
         }
+
         if (shouldSwitch) {
+
             rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+
             switching = true;
+
             switchcount++;
+
         } else {
+
             if (switchcount == 0 && dir == "asc") {
+
                 dir = "desc";
+
                 switching = true;
+
             }
+
         }
+
     }
+
 }
 
-function sortTablePrice(n) {
+function sortByPrice(n) {
+
     var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
+
     table = document.getElementById("myTable");
+
     switching = true;
+
     dir = "asc";
 
     while (switching) {
+
         switching = false;
+
         rows = table.rows;
+
         for (i = 1; i < (rows.length - 1); i++) {
+
             shouldSwitch = false;
+
             x = rows[i].getElementsByTagName("TD")[n];
+
             y = rows[i + 1].getElementsByTagName("TD")[n];
 
             if (dir == "asc") {
+
                 if (Number(x.innerHTML) > Number(y.innerHTML)) {
+
                     shouldSwitch = true;
+
                     break;
+
                 }
+
             } else if (dir == "desc") {
+
                 if (Number(x.innerHTML) < Number(y.innerHTML)) {
+
                     shouldSwitch = true;
+
                     break;
+
                 }
+
             }
+
         }
 
         if (shouldSwitch) {
+
             rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+
             switching = true;
+
             switchcount++;
+
         } else {
+
             if (switchcount == 0 && dir == "asc") {
+
                 dir = "desc";
+
                 switching = true;
+
             }
+
         }
+
     }
+
 }
 
-function myFunction() {
+function searchForName() {
+
     var input, filter, table, tr, td, i, txtValue;
+
     input = document.getElementById("myInput");
+
     filter = input.value.toUpperCase();
+
     table = document.getElementById("myTable");
+
     tr = table.getElementsByTagName("tr");
+
     for (i = 0; i < tr.length; i++) {
+
         td = tr[i].getElementsByTagName("td")[0];
+
         if (td) {
+
             txtValue = td.textContent || td.innerText;
+
             if (txtValue.toUpperCase().indexOf(filter) > -1) {
+
                 tr[i].style.display = "";
+
             } else {
+
                 tr[i].style.display = "none";
+
             }
+
         }
+
     }
+
 }
